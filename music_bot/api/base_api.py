63598293e80af406411bc47b299649dc4af24fc7
s1q <39670960+s1q@users.noreply.github.com>
2021-02-19 04:39:31 +0300

    Add comments to BaseApi class

--- music_bot/api/base_api.py
+++ music_bot/api/base_api.py
@@ -3,9 +3,12 @@ import aiohttp
 
 
 class BaseApi(ABCMeta):
+    """Implementation of Base Music Services API Abstract class."""
+
     def __init__(self, client: aiohttp.ClientSession):
         self._client = client
 
     @abstractmethod
     async def search(self, query: str):
+        """Make asynchronous search query to API Server via aiohttp.ClientSession"""
         pass
