354ab652eaab724721a390bf82c205e546025e77
s1q <39670960+s1q@users.noreply.github.com>
2021-02-19 04:30:44 +0300

    Implement base API class

--- music_bot/api/base_api.py
+++ music_bot/api/base_api.py
@@ -1,5 +1,11 @@
 from abc import ABCMeta, abstractmethod
+import aiohttp
 
 
 class BaseApi(ABCMeta):
-    pass
+    def __init__(self, client: aiohttp.ClientSession):
+        self._client = client
+
+    @abstractmethod
+    async def search(self, query: str):
+        pass
