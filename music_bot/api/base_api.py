8074c42e4ab4dda120c3be694794498906ea18dc
s1q <39670960+s1q@users.noreply.github.com>
2021-02-19 05:36:05 +0300

    Add type annotations to BaseApi class

--- music_bot/api/base_api.py
+++ music_bot/api/base_api.py
@@ -1,14 +1,15 @@
 from abc import ABCMeta, abstractmethod
+from collections.abc import Awaitable
 import aiohttp
 
 
 class BaseApi(ABCMeta):
     """Implementation of Base Music Services API Abstract class."""
 
-    def __init__(self, client: aiohttp.ClientSession):
+    def __init__(self, client: aiohttp.ClientSession) -> None:
         self._client = client
 
     @abstractmethod
-    async def search(self, query: str):
+    async def search(self, query: str) -> Awaitable[dict]:
         """Make asynchronous search query to API Server via aiohttp.ClientSession"""
         pass
